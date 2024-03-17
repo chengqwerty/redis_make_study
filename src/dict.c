1c66fca36a7ff596cca910d23405707c8b28cf03
chengcheng <shmily_think@126.com>
2024-03-17 22:30:57 +0800

    2024-03-17

--- src/dict.c
+++ src/dict.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "dict.h"
 
+/*----------------------------------- types -----------------------------------*/
 struct dictEntry
 {
     void *val;
@@ -14,6 +15,65 @@ struct dictEntry
     struct dictEntry *next;
 };
 
+typedef struct 
+{
+    void *key;
+    dictEntry *next;
+} dictEntryNoValue;
+
+/*----------------------------------- private prototypes -----------------------------------*/
+
+static int _dictInit(dict *d, dictType *type);
+
+/*----------------------------------- hash functions -----------------------------------*/
+
+static uint8_t dict_hash_function_seed[16];
+
+void dictSetHashFunctionSeed(uint8_t *seed)
+{
+    memcpy(dict_hash_function_seed, seed, sizeof(dict_hash_function_seed));
+}
+
+uint8_t *dictGetHashFunctionSeed(void)
+{
+    return dict_hash_function_seed;
+}
+
+uint64_t siphash(const uint8_t *in, const size_t inlen, const uint8_t *k);
+uint64_t siphash_nocase(const uint8_t *in, const size_t inlen, const uint8_t *k);
+
+/* Reset hash table parameters already initialized with _dictInit() */
+static void _dictReset(dict *d, int htidx)
+{
+    d->ht_table[htidx] = NULL;
+    d->ht_size_exp[htidx] = -1;
+    d->ht_used[htidx] = 0;
+}
+
+/* Create a new hash table */
+dict *dictCreate(dictType *type)
+{
+    size_t metasize = type->dictMetadataBytes ? type->dictMetadataBytes(NULL) : 0;
+    dict *d = zmalloc(sizeof(*d) + metasize);
+    if (metasize >0)
+    {
+        memset(dictMetadata(d), 0, metasize);
+    }
+    _dictInit(d, type);
+    return d;
+}
+
+/* Initialize the hash table */
+int _dictInit(dict *d, dictType *type)
+{
+    _dictReset(d, 0);
+    _dictReset(d, 1);
+    d->type = type;
+    d->rehashidx = -1;
+    d->pauserehash = 0;
+    d->pauseAutoResize = 0;
+    return DICT_OK;
+}
 
 void dict_test()
 {
--- src/dict.h
+++ src/dict.h
@@ -1,1 +1,45 @@
+#define DICT_OK 0
+#define DICT_ERR 1
+
+typedef struct dictEntry dictEntry;
+typedef struct dict dict;
+
+typedef struct dictType {
+    uint64_t (*hashFunction)(const void *key);
+    void *(*keyDup)(dict *d, const void *key);
+    void *(*valDup)(dict *d, const void *obj);
+    int (*keyCompare)(dict *d, const void *key1, const void *key2);
+    void (*keyDestructor)(dict *d, void *key);
+    void (*valDestructor)(dict *d, void *obj);
+    int (*resizeAllowed)(size_t moreMem, double usedRatio);
+
+    void (*rehashingStarted)(dict *d);
+    void (*rehashingCompleted)(dict *d);
+    size_t (*dictMetadataBytes)(dict *d);
+
+    void *userdata;
+
+    unsigned int no_value:1;
+    unsigned int keys_are_odd:1;
+} dictType;
+
+struct dict {
+    dictType *type; 
+
+    dictEntry **ht_table[2];
+    unsigned long ht_used[2];
+
+    long rehashidx; /* rehashing not in progress if rehashidx == -1 */
+
+    int16_t pauserehash;
+    signed char ht_size_exp[2];
+    int16_t pauseAutoResize;
+    void *metadata[];
+};
+
+#define dictMetadata(d) (&(d)->metadata)
+
+/* API */
+dict *dictCreate(dictType *type);
+
 void dict_test(void);
